87c478d2536744742df1d7b46d2316e4538cd535
Matteo Raffaele De Silvestri <matteoraffaeledesilvestri@gmail.com>
2021-04-30 01:46:05 +0200

    Midnight review

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 import cv2
 import numpy as np
+import os
 
 # Detect camera from GUI
-camera = 'video/camera_01.mp4'
+camera = 'video/drone.mp4'
 
 cap = cv2.VideoCapture(camera)
 
@@ -16,35 +17,49 @@ while True:
     if frame is None:
         break
 
+    frame = cv2.resize(frame, (1280, 720))
+
     # Draw detection line on the main frame
-    cv2.line(frame, (5, 570), (560, 570), (255, 0, 0), 2)
-    cv2.putText(frame, 'DETECTION POINT', (5, 600), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-    cv2.line(frame, (668, 400), (910, 400), (0, 0, 255), 2)
-    cv2.putText(frame, 'DETECTION POINT', (668, 390), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+    cv2.line(frame, (455, 600), (640, 600), (0, 0, 255), 2)
+    # cv2.line(frame, (520, 390), (650, 390), (255, 0, 0), 1)
+    # cv2.line(frame, (300, 600), (390, 680), (0, 0, 255), 2)
+    # cv2.line(frame, (668, 400), (910, 400), (0, 0, 255), 2)
 
     # Define region of interest (ROI)
-    roi = frame[380 : 570, 0 : 600] # <--- THIS VALUE IS DIFFERENT DEPENDING ON THE CAMERAS
+    roi = frame[250 : 710, 390 : 670] # <--- THIS VALUE IS DIFFERENT DEPENDING ON THE CAMERAS
     
     # Generate mask for object
     mask = object_detector.apply(roi)
 
-    # Findt contours for vehicle
-    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # Remove shadow and other noise from the image
+    _, mask = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
     
+    # Find contours for moving object
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
     # Draw contours
     for cnt in contours:
 
         # Calculate areas and remove small elements
         area = cv2.contourArea(cnt)
-        
-        if area >= 500: # <--- THIS VALUE CAN BE DIFFERENT DEPENDING ON THE CAMERAS
-            cv2.drawContours(frame, [cnt], -1, (0, 255, 0), 1) 
-            # x, y, w, h = cv2.boundingRect(cnt)
-            # cv2.rectangle(roi, (x, y), (x + w, y + y), (0, 255, 0), 1)
-    
+
+        if area >= 1000: # <--- THIS VALUE CAN BE DIFFERENT DEPENDING THE CAMERAS
+
+            cv2.drawContours(roi, [cnt], -1, (0, 0, 255), 1) 
+            x, y, w, h = cv2.boundingRect(cnt)
+            # cv2.rectangle(roi, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+            # Animate detection point
+            if 600 in range(x + 470, y + 470):
+                cv2.line(frame, (455, 600), (640, 600), (255, 255, 255), 2)
+
+            # if y + h == 470 and not os.path.isfile('photos/Detection{}.png'.format(car_id - 1)):
+            #         cv2.imwrite('photos/Detection{}.png'.format(car_id), frame[250 : 720, 390 : 670])
+            #         car_id += 1
+
     # Show the video (and layer)
-    cv2.imshow(camera[6:len(camera) - 4], frame)
-    cv2.imshow('ROI {}'.format(camera[6:len(camera) - 4]), mask)
+    cv2.imshow(camera[6 : len(camera) - 4], frame)
+    cv2.imshow('ROI {}'.format(camera[6 : len(camera) - 4]), mask)
     
     # Wait ESC key to stop the video
     key = cv2.waitKey(30)
