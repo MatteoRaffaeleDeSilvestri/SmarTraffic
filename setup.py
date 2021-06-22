50fc9e4e46f89403817561d5770563c2e9708dfa
Matteo Raffaele De Silvestri <matteoraffaeledesilvestri@gmail.com>
2021-06-22 15:27:43 +0200

    Update 22.06

--- setup.py
+++ setup.py
@@ -6,23 +6,22 @@ if __name__ == '__main__':
 	try:
 		print('Downloading file...')
 		
-		files = ['https://speed.hetzner.de/100MB.bin',
-				'https://github.com/MatteoRaffaeleDeSilvestri/SmarTraffic/raw/master/video/camera_1.mp4',
-				'https://github.com/MatteoRaffaeleDeSilvestri/SmarTraffic/raw/master/video/camera_2.mp4',
-				'https://github.com/MatteoRaffaeleDeSilvestri/SmarTraffic/raw/master/video/camera_3.mp4',
-				'https://github.com/MatteoRaffaeleDeSilvestri/SmarTraffic/raw/master/video/camera_4.mp4',
-				'https://github.com/MatteoRaffaeleDeSilvestri/SmarTraffic/raw/master/yolo/yolov4.weights']
+		files = ['https://drive.google.com/file/d/1fjvPwop2IUxcWLsdzEy5fe9yPIj1jfa-/view?usp=sharing',
+				 'https://github.com/MatteoRaffaeleDeSilvestri/SmarTraffic/raw/master/video/camera_2.mp4',
+				 'https://github.com/MatteoRaffaeleDeSilvestri/SmarTraffic/raw/master/video/camera_3.mp4',
+				 'https://github.com/MatteoRaffaeleDeSilvestri/SmarTraffic/raw/master/video/camera_4.mp4',
+				 'https://github.com/MatteoRaffaeleDeSilvestri/SmarTraffic/raw/master/yolo/yolov4.weights']
 
 		for url in files:
 
 			req = requests.get(url, stream = True)
 
 			name = url.split('/')[-1]
-			perc = int(req.headers['content-length']) / 1024
 			folder = url.split('/')[-2]
+			print(name, folder)
 
 			with open('{}/{}'.format(folder, name), 'wb') as f:
-				for data in tqdm(desc = name, iterable = req.iter_content(chunk_size = 1024), total = perc, unit = ' Kb', mininterval = 0.5, maxinterval= 1):
+				for data in tqdm(desc = name, iterable = req.iter_content(chunk_size = 1024), total = int(req.headers['content-length']) / 1024, unit = ' Kb', mininterval = 0.5, maxinterval= 1):
 					f.write(data)
 
 		print('Operation completed successfully')
